d2f3339cbac798b3e43c098359ee5b422e4c8a4a
snowhyt <oliverglorioso.44@gmail.com>
2025-04-11 00:12:27 +0800

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Routes, Route} from 'react-router-dom';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
+  
     <>
-      <div>
-      {/* tailwind testing */}
-        <h1 className='text-red-500 text-4xl font-bold'>MPCC-SYSTEM</h1> 
+    <Routes>
+      <Route path="/login" element={Login}/>
+      <Route path="/signup" element={Signup}/>
+      <Route path="/dashboard" element={Dashboard}/>
 
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    
+    </Routes>
+    
     </>
+  
+     
   )
 }
+
 
 export default App
