2620a7d7d9ee712ab1964efcbdb8fd6a01368244
Oliver <oliverglorioso.44@gmail.com>
2025-04-14 16:09:23 +0800

    add crud unfinished, reset docker

--- backend/src/models/employee.model.js
+++ backend/src/models/employee.model.js
@@ -6,10 +6,45 @@ import { Sequelize } from "sequelize";
 const Employee = db.define('employee',{
     id:{
         primaryKey: true,
+        fname: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        lname: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        emp_id:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        password:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        }, 
+        role:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        profilepicture:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        }, 
+        phone:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+
     },
     
 
 
 
 
-})
+});
+
+export default Employee;
